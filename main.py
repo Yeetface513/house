9eee21b9c1b678c150d042d2ae38b417a79a633c
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2024-02-08 18:29:37 +0000

    beans

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ def intro():
      enter = str(input("Do you want to enter the house? (yes/no) "))
      if enter == "yes":
          print("You are now inside the house.")
+         name = str(input("What is your name?\n"))
      else:
          print("Goodbye!")
          sys.exit()
 
-house = 
+rooms = {
+    "door": 0,
+    "currydoor": 1,
+    "kitchen": 2,
+    "Livingroom": 3,
+    "maccas": 4,
+    }
+intro()
+def game():
+    roomnumber = int(input("What room do you want to go to? (1, 2, 3, 4 and 0 to leave) "))
+    if roomnumber = 0:
+        print("You have left the house.")
+        sys.exit()
+    else:
+        print()
+
 
