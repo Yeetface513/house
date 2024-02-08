f1a3c4821297b0da02c78aeb896abd079aa3b19b
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2024-02-08 18:07:21 +0000

    beans

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+def intro():
+     print("Welcome to haunted house, it is  very spooky scary skeleton!")
+     print("You are standing outside a haunted house.")
+     print("There is a door in front of you.")
+     enter = str(input("Do you want to enter the house? (yes/no) "))
+     if enter == "yes":
+         print("You are now inside the house.")
+     else:
+         print("Goodbye!")
+         sys.exit()
+
+house = 
+
