587ac4d92eb1caca29b8c6d6c98bdcb741eed4a2
Yeetface513 <123550198+Yeetface513@users.noreply.github.com>
2024-03-21 09:38:38 +0000

    fix

--- maim.py
+++ maim.py
@@ -21,32 +21,34 @@ def intro():
     print("You are standing outside a haunted house.")
     print("There is a door in front of you.")
     enter = str(input("Do you want to enter the house? (yes/no) "))
-    if enter == "yes":
+    if enter == "yes": 
         print("You are now inside the house.")
         name = str(input("What is your name?\n")) # I have not used this yet, maybe for a bossfight or something?
-        email = str(input("What is your email adress?\n"))
-        try: # See if the file exists already
-            with open('data.txt', 'x') as l: # X means it will create a new file
+        emailbool = str(input("Do you want to sign up to our mailing list (y/n)? \n"))
+        if emailbool =="y":
+            email = str(input("What is your email adress?\n"))
+            try: # See if the file exists already
+                with open('data.txt', 'x') as l: # X means it will create a new file
+                    pass
+            except FileExistsError: # This error is if the file already exists
                 pass
-        except FileExistsError: # This error is if the file already exists
-            pass
-        try:
-            with open('data.txt', 'a') as f: # A means append which doesn't overwrite everything in the file unlike w which is short for write
-                f.write(email + '\n') # \n is just for formatting
-            with open('data.txt', 'r') as e: # R means read which means you can't edit it, just read it
-                data = e.read()
-            body=f"Hi, {name}, BUY THIS AMAZING PRODUCT FROM CAFE GEORGE NOW, IT IS NOW 1000% OFF IN OUR FOREVER INFINITE SALE \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n smallprint: by viewing this email, you have signed your life away to cafe george." # your message body goes here
-            subject="Cafe George" # Subject goes here
-            recipients=[data] # List of  recipients
-            smtpObj = smtplib.SMTP('smtp.office365.com', 587) # Object init
-            smtpObj.ehlo() # Handshake
-            smtpObj.starttls() # Encryption
-            smtpObj.login('cafe_george@outlook.com', "Messaging1") # Also outlook login details
-            smtpObj.sendmail('cafe_george@outlook.com', recipients, f"Subject: {subject}\n\n{body}") # Create & send the email
-            smtpObj.sendmail('cafe_george@outlook.com','cafe_george@outlook.com',f"Subject: The Current Mailing List \n\n{data}")
-            smtpObj.quit() # Terminate session
-        except FileExistsError and SyntaxError:
-            pass
+            try:
+                with open('data.txt', 'a') as f: # A means append which doesn't overwrite everything in the file unlike w which is short for write
+                    f.write(email + '\n') # \n is just for formatting
+                with open('data.txt', 'r') as e: # R means read which means you can't edit it, just read it
+                    data = e.read()
+                body=f"Hi, {name}, BUY THIS AMAZING PRODUCT FROM CAFE GEORGE NOW, IT IS NOW 1000% OFF IN OUR FOREVER INFINITE SALE \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n smallprint: by viewing this email, you have signed your life away to cafe george." # your message body goes here
+                subject="Cafe George" # Subject goes here
+                recipients=[data] # List of  recipients
+                smtpObj = smtplib.SMTP('smtp.office365.com', 587) # Object init
+                smtpObj.ehlo() # Handshake
+                smtpObj.starttls() # Encryption
+                smtpObj.login('cafe_george@outlook.com', "Messaging1") # Also outlook login details
+                smtpObj.sendmail('cafe_george@outlook.com', recipients, f"Subject: {subject}\n\n{body}") # Create & send the email
+                smtpObj.sendmail('cafe_george@outlook.com','cafe_george@outlook.com',f"Subject: The Current Mailing List \n\n{data}")
+                smtpObj.quit() # Terminate session
+            except FileExistsError and SyntaxError:
+                pass
     else:
         print("Goodbye!")
         sys.exit()
